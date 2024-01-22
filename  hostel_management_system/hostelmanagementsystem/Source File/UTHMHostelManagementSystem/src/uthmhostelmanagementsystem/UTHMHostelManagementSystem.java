f565bb30043f81de41880758eba623e581bf05e0
sivaneeswarankrishnan <149932844+sivaneeswarankrishnan@users.noreply.github.com>
2024-01-22 15:01:21 +0800

    unzipgit

---
+++  hostel_management_system/hostelmanagementsystem/Source File/UTHMHostelManagementSystem/src/uthmhostelmanagementsystem/UTHMHostelManagementSystem.java
@@ -0,0 +1,17 @@
+package uthmhostelmanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sivan
+ */
+public class UTHMHostelManagementSystem {
+
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+    }
+    
+}
